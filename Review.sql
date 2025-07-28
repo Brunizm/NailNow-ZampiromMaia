0ae1e9e9a5f602d9aab5257e4a2baaf0200349b7
Bruna <86904223+Brunizm@users.noreply.github.com>
2025-07-28 19:43:39 -0300

    Add files via upload

---
+++ Review.sql
@@ -0,0 +1,66 @@
+/*
+-- Query: SELECT * FROM NailNow.Review
+LIMIT 0, 1000
+
+-- Date: 2025-07-28 19:43
+*/
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (1,1,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (2,2,5,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (3,5,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (4,9,4,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (5,10,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (6,11,4,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (7,18,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (8,21,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (9,26,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (10,28,5,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (11,29,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (12,32,4,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (13,37,4,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (14,40,5,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (15,42,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (16,43,4,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (17,44,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (18,45,5,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (19,47,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (20,48,5,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (21,51,4,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (22,64,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (23,66,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (24,67,4,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (25,68,4,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (26,80,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (27,95,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (28,99,5,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (29,100,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (30,102,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (31,103,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (32,109,4,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (33,112,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (34,114,5,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (35,118,5,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (36,127,4,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (37,130,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (38,139,4,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (39,140,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (40,141,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (41,143,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (42,144,5,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (43,146,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (44,150,5,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (45,152,5,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (46,153,5,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (47,157,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (48,169,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (49,171,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (50,173,5,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (51,177,4,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (52,180,4,'Muito satisfeita com o serviço!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (53,182,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (54,184,5,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (55,185,4,'Atendimento excelente!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (56,187,5,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (57,190,4,'Voltaria com certeza!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (58,193,5,'Adorei o resultado final!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (59,199,4,'Profissional incrível!');
+INSERT INTO `` (`IdReview`,`IdAppointment`,`Rating`,`Comment`) VALUES (60,1,5,'Servicio excelente, muy puntual y profesional.');
---
+++ Service.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM NailNow.Service
+LIMIT 0, 1000
+
+-- Date: 2025-07-28 19:43
+*/
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (1,'EsmaltadoGel','unha gel',95.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (2,'Manicure','Fazer maos',60.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (3,'Pedicuria','Fazer Pés',48.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (4,'Alongamento','unha alongada',150.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (5,'MaoePe','mao e pé',100.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (6,'Spa','Spa Pés',250.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (7,'Podologia',NULL,160.00);
+INSERT INTO `` (`IdService`,`Name`,`Description`,`Price`) VALUES (8,'Podología','Tratamiento especializado para pies',160.00);
